5a82c1ed7602b4aed914fdfc4aa6ac319fa6fdd7
Stefan Saraev <stefan@saraev.ca>
2014-01-04 00:17:44 +0200

    libmtp: convert to new package format

--- lib/libmtp/package.mk
+++ lib/libmtp/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,16 +18,27 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="libmtp"
+PKG_VERSION="1.1.6"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://libmtp.sourceforge.net/"
+PKG_URL="$SOURCEFORGE_SRC/project/$PKG_NAME/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain libusb"
+PKG_SECTION="libs"
+PKG_PRIORITY="optional"
+PKG_SHORTDESC="MTP library"
+PKG_LONGDESC="libmtp is an Initiator implementation of the Media Transfer Protocol (MTP) in the form of a library suitable primarily for POSIX compliant operating systems. We implement MTP Basic, the stuff proposed for standardization."
 
-cd $PKG_BUILD
-ac_cv_func_malloc_0_nonnull=yes \
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --sysconfdir=/etc \
+PKG_IS_ADDON="no"
+
+PKG_AUTORECONF="yes"
+
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
             --disable-shared \
             --enable-static \
-            --disable-mtpz \
-
-$MAKEINSTALL
+            --disable-mtpz"
