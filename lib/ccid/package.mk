edebfe6b3f24935a8bfdd66c7d1cfa4a46b3d31d
Stefan Saraev <stefan@saraev.ca>
2014-01-04 01:12:55 +0200

    ccid: convert to new package format

--- lib/ccid/package.mk
+++ lib/ccid/package.mk
@@ -25,8 +25,8 @@ PKG_ARCH="any"
 PKG_LICENSE="LGPL"
 PKG_SITE="http://pcsclite.alioth.debian.org/ccid.html"
 PKG_URL="https://alioth.debian.org/frs/download.php/3897/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
-PKG_DEPENDS=""
-PKG_BUILD_DEPENDS="toolchain pcsc-lite"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain pcsc-lite"
 PKG_PRIORITY="optional"
 PKG_SECTION="driver"
 PKG_SHORTDESC="CCID free software driver"
@@ -35,3 +35,10 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static --enable-twinserial"
+
+make_target() {
+  make
+  make -C src/ Info.plist
+}
