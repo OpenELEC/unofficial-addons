b7c91dddb084e5cd515913e6ef5a6823655e1091
Stefan Saraev <stefan@saraev.ca>
2014-01-05 15:34:01 +0200

    acpica: convert to new package format

--- addons/debugging/tools/acpica/package.mk
+++ addons/debugging/tools/acpica/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2013 Dag Wieers (dag@wieers.com)
@@ -20,14 +18,33 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="acpica"
+PKG_VERSION="unix2-20130328"
+PKG_REV="0"
+PKG_ARCH="i386 x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.acpica.org/"
+PKG_URL="https://www.acpica.org/download/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://acpica.org/sites/acpica/files/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain flex bison:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="debug/tools"
+PKG_SHORTDESC="acpica: A set of tools to disassemble ACPI tables"
+PKG_LONGDESC="acpica is a set of tools from Intel to disassemble ACPI tables."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Dag Wieers (dag@wieers.com)"
 
 [ "$TARGET_ARCH" = "i386" ] && ACPICA_BITS="32"
 [ "$TARGET_ARCH" = "x86_64" ] && ACPICA_BITS="64"
 
-cd $PKG_BUILD
-
-make PREFIX=/usr \
+make_target() {
+  make PREFIX=/usr \
      CC="$TARGET_CC" \
      AR="$TARGET_AR" \
      HOST=_LINUX \
@@ -35,4 +52,16 @@ make PREFIX=/usr \
      BITS=$ACPICA_BITS \
      YACC=$ROOT/$TOOLCHAIN/bin/bison \
      CWARNINGFLAGS="-O2 $TARGET_CFLAGS"
+}
 
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  [ "$TARGET_ARCH" = "i386" ] && ACPICA_BIN="bin32"
+  [ "$TARGET_ARCH" = "x86_64" ] && ACPICA_BIN="bin64"
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -p $PKG_BUILD/generate/unix/$ACPICA_BIN/* $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
