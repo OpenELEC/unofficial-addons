f3e5fec1c9abad84a726f6b18ef8ae44402f6af9
Stefan Saraev <stefan@saraev.ca>
2014-01-05 15:59:01 +0200

    transmission: convert to new package format

--- addons/service/downloadmanager/transmission/package.mk
+++ addons/service/downloadmanager/transmission/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,23 +18,47 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="transmission"
+PKG_VERSION="2.82"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="http://www.transmissionbt.com/"
+PKG_URL="http://download.transmissionbt.com/files/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain zlib openssl curl libevent"
+PKG_PRIORITY="optional"
+PKG_SECTION="service/downloadmanager"
+PKG_SHORTDESC="transmission: a fast, easy and free BitTorrent client"
+PKG_LONGDESC="transmission is a fast, easy and free BitTorrent client"
 
-cd $PKG_BUILD
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --sysconfdir=/etc \
-            --localstatedir=/var \
-            --disable-static \
-            --enable-shared \
-            --enable-utp \
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.service"
+
+PKG_AUTORECONF="yes"
+
+PKG_MAINTAINER="unofficial.addon.pro"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-utp \
             --enable-largefile \
             --disable-nls \
             --disable-cli \
             --disable-mac \
             --enable-lightweight \
             --enable-daemon \
-            --with-gnu-ld \
+            --with-gnu-ld"
 
-make V=1
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp $PKG_BUILD/.$TARGET_NAME/daemon/transmission-daemon $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp $PKG_BUILD/.$TARGET_NAME/daemon/transmission-remote $ADDON_BUILD/$PKG_ADDON_ID/bin
+
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/web
+  cp -R $PKG_BUILD/.$TARGET_NAME/web/* $ADDON_BUILD/$PKG_ADDON_ID/web
+  find $ADDON_BUILD/$PKG_ADDON_ID/web -name "Makefile*" -exec rm -rf {} ";"
+  rm -rf $ADDON_BUILD/$PKG_ADDON_ID/web/LICENSE
+}
