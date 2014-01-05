b06dd750ac72afcdb089f4fc2f41c82a03ac0680
Stefan Saraev <stefan@saraev.ca>
2014-01-05 14:05:29 +0200

    libzen: convert to new package format

--- lib/libzen/package.mk
+++ lib/libzen/package.mk
@@ -1,8 +1,7 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2011 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2011-2011 Gregor Fuis (gujs@openelec.tv)
 #
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,14 +16,30 @@
 #  You should have received a copy of the GNU General Public License
 #  along with OpenELEC.tv; see the file COPYING.  If not, write to
 #  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  http://www.gnu.org/copyleft/gpl.htmlLooking for the latest version?
 ################################################################################
 
-. config/options $1
+PKG_NAME="libzen"
+PKG_VERSION="0.4.29"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://mediaarea.net/en/MediaInfo/"
+PKG_URL="http://mediaarea.net/download/source/libzen/$PKG_VERSION/libzen_$PKG_VERSION.tar.bz2"
+PKG_SOURCE_DIR="ZenLib"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="multimedia"
+PKG_SHORTDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
+PKG_LONGDESC="MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files"
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
-cd $PKG_BUILD
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
 
-cd Project/GNU/Library
+make_target() {
+  cd Project/GNU/Library
   do_autoreconf
   ./configure \
         --host=$TARGET_NAME \
@@ -34,7 +49,9 @@ cd Project/GNU/Library
         --prefix=/usr
 
   make
-  $MAKEINSTALL
+}
+
+post_makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/include/ZenLib
   cp -aP ../../../Source/ZenLib/*.h $SYSROOT_PREFIX/usr/include/ZenLib
   for i in HTTP_Client Format/Html Format/Http ; do
@@ -42,3 +59,4 @@ cd Project/GNU/Library
     cp -aP ../../../Source/ZenLib/$i/*.h $SYSROOT_PREFIX/usr/include/ZenLib/$i/
   done
   cp -P libzen-config $ROOT/$TOOLCHAIN/bin
+}
