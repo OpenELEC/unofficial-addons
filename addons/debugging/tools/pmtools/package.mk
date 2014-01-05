4a8c53f2d1daad928eab3f3be22ad90ed3bb6812
Stefan Saraev <stefan@saraev.ca>
2014-01-05 15:36:13 +0200

    pmtools: convert to new package format

--- addons/debugging/tools/pmtools/package.mk
+++ addons/debugging/tools/pmtools/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2013 Dag Wieers (dag@wieers.com)
@@ -20,18 +18,49 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="pmtools"
+PKG_VERSION="20110323"
+PKG_REV="0"
+PKG_ARCH="i386 x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://lesswatts.org/projects/acpi/utilities.php"
+PKG_URL="http://mirror.linux.org.au/linux/kernel/people/lenb/acpi/utils/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_SOURCE_DIR="${PKG_NAME}"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="debug/tools"
+PKG_SHORTDESC="pmtools: ACPI debugging utilities"
+PKG_LONGDESC="The pmtools package contains tools to debug ACPI DSDT tables"
 
-cd $PKG_BUILD
-make CC="$TARGET_CC" \
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Dag Wieers (dag@wieers.com)"
+
+make_target() {
+  make CC="$TARGET_CC" \
      CFLAGS="$TARGET_CFLAGS -Wall -Wstrict-prototypes -Wdeclaration-after-statement -Os -s -D_LINUX -DDEFINE_ALTERNATE_TYPES -I../include" \
      -C acpidump acpidump
 
-make CC="$TARGET_CC" \
+  make CC="$TARGET_CC" \
      CFLAGS="$TARGET_CFLAGS -Wall -Wstrict-prototypes -Wdeclaration-after-statement -Os -s -D_LINUX -DDEFINE_ALTERNATE_TYPES -I../include" \
      -C acpixtract acpixtract
 
-make CC="$TARGET_CC" \
+  make CC="$TARGET_CC" \
      CFLAGS="$TARGET_CFLAGS -Wall -Wstrict-prototypes -Wdeclaration-after-statement -Os -s -D_LINUX -DDEFINE_ALTERNATE_TYPES -I../include" \
      -C madt madt
+}
 
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/acpidump/acpidump $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/acpixtract/acpixtract $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/madt/madt $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
