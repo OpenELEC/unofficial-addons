ed1f9e007cb17d954b62cecbad2881f22b9f595f
Stefan Saraev <stefan@saraev.ca>
2014-01-05 17:12:56 +0200

    noad: convert to new package format

--- addons/tools/noad/package.mk
+++ addons/tools/noad/package.mk
@@ -1,8 +1,7 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2011 Anthony Nash (nash.ant@gmail.com)
 #
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,7 +19,36 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options
+PKG_NAME="noad"
+PKG_VERSION="0.8.5"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://noad.net23.net/"
+PKG_URL="http://noad.net23.net/noad-0.8.5.tar.bz2"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="ffmpeg libmpeg2"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="noad - no advertising"
+PKG_LONGDESC="noad - no advertising"
 
-mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp $PKG_BUILD/noad $ADDON_BUILD/$PKG_ADDON_ID/bin
+PKG_IS_ADDON="yes"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="yes"
+
+PKG_MAINTAINER="unofficial.addon.pro, henkwiedig"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-magick --with-tools"
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp $PKG_BUILD/.$TARGET_NAME/noad $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
