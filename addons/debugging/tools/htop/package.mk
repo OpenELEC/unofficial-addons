9b1b54ee9b7b4424f74b95b5b2026d0b4e1f0fda
Stefan Saraev <stefan@saraev.ca>
2014-01-05 15:35:40 +0200

    htop: convert to new package format

--- addons/debugging/tools/htop/package.mk
+++ addons/debugging/tools/htop/package.mk
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
@@ -20,24 +18,51 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="htop"
+PKG_VERSION="1.0.2"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://htop.sourceforge.net/"
+PKG_URL="$SOURCEFORGE_SRC/$PKG_NAME/$PKG_NAME/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain ncurses"
+PKG_PRIORITY="optional"
+PKG_SECTION="debug/tools"
+PKG_SHORTDESC="htop: Htop is an ncurses based interactive process viewer for Linux."
+PKG_LONGDESC="Htop is an ncurses based interactive process viewer for Linux."
 
-LDFLAGS="$LDFLAGS -ltinfo"
-CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/ncurses"
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
 
-cd $PKG_BUILD
-ac_cv_func_malloc_0_nonnull=yes \
-ac_cv_func_realloc_0_nonnull=yes \
-./configure --host=$TARGET_NAME \
-            --build=$HOST_NAME \
-            --prefix=/usr \
-            --sysconfdir=/etc \
-            --localstatedir=/var \
+PKG_AUTORECONF="yes"
+
+PKG_MAINTAINER="unofficial.addon.pro"
+
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
+            ac_cv_func_realloc_0_nonnull=yes \
             --enable-cgroup \
             --disable-vserver \
             --disable-unicode \
             --disable-native-affinity \
             --disable-hwloc \
-            --with-gnu-ld
+            --with-gnu-ld"
 
-make
+pre_configure_target() {
+  export LDFLAGS="$LDFLAGS -ltinfo"
+  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/ncurses"
+}
+
+pre_build_target() {
+  mkdir -p $PKG_BUILD/.$TARGET_NAME
+  cp -RP $PKG_BUILD/* $PKG_BUILD/.$TARGET_NAME
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/.$TARGET_NAME/htop $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
