72de11cd15e268c89acf92d782bb486476259703
Stefan Saraev <stefan@saraev.ca>
2014-01-05 14:07:57 +0200

    fdupes: convert to new package format

--- addons/tools/fdupes/package.mk
+++ addons/tools/fdupes/package.mk
@@ -1,8 +1,7 @@
-#!/bin/sh
-
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2011 Anthony Nash (nash.ant@gmail.com)
 #
 #  This Program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -20,7 +19,33 @@
 #  http://www.gnu.org/copyleft/gpl.html
 ################################################################################
 
-. config/options $1
+PKG_NAME="fdupes"
+PKG_VERSION="1.50-PR2"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://premium.caribe.net/~adrian2/fdupes.html"
+PKG_URL="http://premium.caribe.net/~adrian2/programs/fdupes/beta/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="FDUPES is a program for identifying or deleting duplicate files residing within specified directories"
+PKG_LONGDESC="FDUPES is a program for identifying or deleting duplicate files residing within specified directories"
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
 
-mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
   cp -P $PKG_BUILD/fdupes $ADDON_BUILD/$PKG_ADDON_ID/bin/
+}
