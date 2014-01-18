f309a5725b138c0d104191e8edeef250da933023
Stefan Saraev <stefan@saraev.ca>
2014-01-18 23:15:27 +0200

    new addon: 'hid_mapper'

---
+++ addons/tools/hid_mapper/package.mk
@@ -0,0 +1,48 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="hid_mapper"
+PKG_VERSION="0"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE=""
+PKG_URL="http://www.coldsource.net/hid_mapper.tar.gz"
+PKG_SOURCE_DIR="${PKG_NAME}_beta"
+PKG_DEPENDS_TARGET=""
+PKG_BUILD_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="network"
+PKG_SHORTDESC="generic HID mapper"
+PKG_LONGDESC="generic HID mapper"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+makeinstall_target() {
+  : # nope
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/hid_mapper $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
