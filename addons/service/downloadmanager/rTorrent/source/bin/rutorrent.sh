348206fc0227b82dbb9b37f57b02e30a64eff77d
Stefan Saraev <stefan@saraev.ca>
2014-03-08 21:57:50 +0200

    rTorrent: fix rutorrent after 9c62a42

--- addons/service/downloadmanager/rTorrent/source/bin/rutorrent.sh
+++ addons/service/downloadmanager/rTorrent/source/bin/rutorrent.sh
@@ -18,9 +18,16 @@ H:$ADDON_DIR/rutorrent/
 A:*                          # Allow address from all ip's
 E404:/usr/www/error/404.html # /path/e404.html is the 404 (not found) error page
 I:index.html                 # Show index.html when a directory is requested
-*.php:$ADDON_DIR/bin/php
+*.php:$ADDON_DIR/bin/php-wrapper
 
 EOF
+
+cat > $ADDON_DIR/bin/php-wrapper << EOF
+#!/bin/sh
+exec $ADDON_DIR/bin/php -c $ADDON_DIR/bin/php.ini
+EOF
+
+chmod a+x $ADDON_DIR/bin/php-wrapper
 
 if [ "$RUTORRENT_AUTH" == "true" ];then
   cat >> $RUTORRENT_CONF << EOF
