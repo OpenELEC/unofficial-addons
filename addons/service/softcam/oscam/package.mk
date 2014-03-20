1ad0102e9f5a87399368177ca4684934ec2ced1d
Stefan Saraev <stefan@saraev.ca>
2014-03-20 23:11:31 +0200

    oscam: update to oscam-9574

--- addons/service/softcam/oscam/package.mk
+++ addons/service/softcam/oscam/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="9501"
-PKG_REV="2"
+PKG_VERSION="9574"
+PKG_REV="3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
