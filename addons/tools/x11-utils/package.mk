462cf908a9c455a9d42ea7acfa40558f9ca9c318
Stefan Saraev <stefan@saraev.ca>
2014-03-23 13:29:40 +0200

    new addon: x11-utils

---
+++ addons/tools/x11-utils/package.mk
@@ -0,0 +1,55 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2011 Anthony Nash (nash.ant@gmail.com)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="x11-utils"
+PKG_VERSION="1"
+PKG_REV="0"
+PKG_ARCH="i386 x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="http://xorg.freedesktop.org/releases/individual/app/"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain xprop xwininfo"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="x11-utils"
+PKG_LONGDESC="xprop xwininfo"
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+make_target() {
+  : nop
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -PR $(get_build_dir xprop)/.$TARGET_NAME/xprop $ADDON_BUILD/$PKG_ADDON_ID/bin/
+  cp -PR $(get_build_dir xwininfo)/.$TARGET_NAME/xwininfo $ADDON_BUILD/$PKG_ADDON_ID/bin/
+}
