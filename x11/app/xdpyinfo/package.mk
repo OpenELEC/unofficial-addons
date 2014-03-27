ea9b99449c415729d3db2622c1bd1a4402b86571
Stefan Saraev <stefan@saraev.ca>
2014-03-27 20:39:52 +0200

    new package: xdpyinfo

---
+++ x11/app/xdpyinfo/package.mk
@@ -0,0 +1,41 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="xdpyinfo"
+PKG_VERSION="1.3.1"
+PKG_REV="1"
+PKG_ARCH=""
+PKG_LICENSE="GPL"
+PKG_SITE="http://xorg.freedesktop.org/releases/individual/app/"
+PKG_URL="http://xorg.freedesktop.org/releases/individual/app/${PKG_NAME}-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libX11 xproto"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/app"
+PKG_SHORTDESC=""
+PKG_LONGDESC=""
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
+
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+makeinstall_target() {
+  : # nothing to do here
+}
