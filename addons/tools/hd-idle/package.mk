992acc4c6afe89761801ca1f274106fba8211fc1
Stefan Saraev <stefan@saraev.ca>
2014-03-31 15:25:14 +0300

    meh. fix bad copyright c/p

--- addons/tools/hd-idle/package.mk
+++ addons/tools/hd-idle/package.mk
@@ -1,7 +1,6 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
 #      Copyright (C) 2009-2011 Stephan Raue (stephan@openelec.tv)
-#      Copyright (C) 2011-2011 Gregor Fuis (gujs@openelec.tv)
 #
 #  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
