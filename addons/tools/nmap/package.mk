055e44f1b1dcee888ef12ce5b181caf5b51f0563
Stefan Saraev <stefan@saraev.ca>
2014-05-17 14:49:14 +0300

    new addon: nmap

---
+++ addons/tools/nmap/package.mk
@@ -0,0 +1,64 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2014 vpeter4 (peter.vicman@gmail.com)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="nmap"
+PKG_VERSION="6.46"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://nmap.org/"
+PKG_URL="http://nmap.org/dist/${PKG_NAME}-${PKG_VERSION}.tgz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="network tool"
+PKG_LONGDESC="Free Security Scanned for Network"
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="none"
+
+PKG_CONFIGURE_OPTS_TARGET="--enable-static \
+                           --with-pcap=linux \
+                           --with-libpcap=included \
+                           --with-libpcre=included \
+                           --with-libdnet=included \
+                           --with-liblua=included \
+                           --with-liblinear=included"
+
+pre_configure_target() {
+# nmap fails to build in subdirs
+  cd $ROOT/$PKG_BUILD
+    rm -rf .$TARGET_NAME
+
+  export CPPFLAGS="$CPPFLAGS -Iliblua"
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/nmap $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
