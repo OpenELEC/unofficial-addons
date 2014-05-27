ebd030eabd238ddf02515d5d8a6d7c330770b443
stbenz <benz.st@gmail.com>
2014-05-27 21:49:00 +0200

    efibootmgr: new addon
    closes #29

---
+++ addons/tools/efibootmgr/package.mk
@@ -0,0 +1,56 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2014 Stefan Benz (benz.st@gmail.com)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="efibootmgr"
+PKG_VERSION="0.7.0"
+PKG_REV="0"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/vathpela/efibootmgr"
+PKG_URL="http://stbenz.de/openelec/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain efivar pciutils zlib"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="EFI Boot Manager"
+PKG_LONGDESC="This is a Linux user-space application to modify the Intel Extensible Firmware Interface (EFI) Boot Manager configuration. This application can create and destroy boot entries, change the boot order, change the next running boot option, and more."
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Stefan Benz (benz.st@gmail.com)"
+
+pre_make_target() {
+  strip_lto
+  export EXTRA_CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include"
+  export LDFLAGS="$LDFLAGS -L$SYSROOT_PREFIX/usr/lib"
+}
+
+makeinstall_target() {
+  : # nop
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+  cp -P $PKG_BUILD/src/efibootmgr/efibootmgr $ADDON_BUILD/$PKG_ADDON_ID/bin/
+}
+
---
+++ lib/efivar/package.mk
@@ -0,0 +1,59 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2014 Stefan Benz (benz.st@gmail.com)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="efivar"
+PKG_VERSION="0.10"
+PKG_REV="0"
+PKG_ARCH="x86_64"
+PKG_LICENSE="LGPL"
+PKG_SITE="https://github.com/vathpela/efivar"
+PKG_URL="http://stbenz.de/openelec/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain efivar:host"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="EFI Variables"
+PKG_LONGDESC="Tools and library to manipulate EFI variables."
+PKG_IS_ADDON="no"
+
+PKG_AUTORECONF="no"
+
+PKG_MAINTAINER="Stefan Benz (benz.st@gmail.com)"
+
+make_host() {
+  make -C src/ makeguids
+}
+
+make_target() {
+  strip_lto
+  make -C src/ libefivar.a efivar-guids.h efivar.h
+}
+
+makeinstall_host() {
+  : # noop
+}
+
+makeinstall_target() {
+  mkdir -p $SYSROOT_PREFIX/usr/lib
+  mkdir -p $SYSROOT_PREFIX/usr/include
+  cp -P src/libefivar.a $SYSROOT_PREFIX/usr/lib/
+  cp -P src/efivar.h $SYSROOT_PREFIX/usr/include/
+  cp -P src/efivar-guids.h $SYSROOT_PREFIX/usr/include/
+}
+
