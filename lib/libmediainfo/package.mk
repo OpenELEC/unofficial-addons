4e6708a67fde0de1a0ecdec2bb5f34344d3c7cec
Stephan Raue <stephan@openelec.tv>
2014-07-20 11:49:21 +0200

    libmediainfo: update to libmediainfo-0.7.69
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- lib/libmediainfo/package.mk
+++ lib/libmediainfo/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libmediainfo"
-PKG_VERSION="0.7.64"
+PKG_VERSION="0.7.69"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
