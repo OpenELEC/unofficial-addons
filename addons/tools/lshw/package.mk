906ce49adda0f6afd547ce82905a421d9de9ca1a
Stephan Raue <stephan@openelec.tv>
2014-07-20 11:39:05 +0200

    lshw: update to lshw-B.02.17, bump addon version
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/lshw/package.mk
+++ addons/tools/lshw/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="lshw"
-PKG_VERSION="B.02.16"
+PKG_VERSION="B.02.17"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
