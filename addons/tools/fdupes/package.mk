4a106bad45012e6e284f2319364b5572728b55c4
Stephan Raue <stephan@openelec.tv>
2014-07-20 11:12:16 +0200

    fdupes: update to fdupes-1.51, bump addon version
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/fdupes/package.mk
+++ addons/tools/fdupes/package.mk
@@ -19,12 +19,12 @@
 ################################################################################
 
 PKG_NAME="fdupes"
-PKG_VERSION="1.50-PR2"
+PKG_VERSION="1.51"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://premium.caribe.net/~adrian2/fdupes.html"
-PKG_URL="http://premium.caribe.net/~adrian2/programs/fdupes/beta/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://fdupes.googlecode.com/files/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="tools"
