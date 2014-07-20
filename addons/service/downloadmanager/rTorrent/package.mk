4c2df1206450e902106c78d0832ac259974295af
Stephan Raue <stephan@openelec.tv>
2014-07-20 13:42:07 +0200

    rTorrent: bump addon version
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/service/downloadmanager/rTorrent/package.mk
+++ addons/service/downloadmanager/rTorrent/package.mk
@@ -19,8 +19,8 @@
 ################################################################################
 
 PKG_NAME="rTorrent"
-PKG_VERSION="4.1"
-PKG_REV="3"
+PKG_VERSION="4.3"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://libtorrent.rakshasa.no"
