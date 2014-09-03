d26c9b28eedd08ca8a15610315d90da4a5db8a2c
Stefan Saraev <stefan@saraev.ca>
2014-09-03 09:59:03 +0300

    new addon: lm_sensors

---
+++ addons/tools/lm_sensors/package.mk
@@ -0,0 +1,54 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="lm_sensors"
+PKG_VERSION="3.3.4"
+PKG_REV="0"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://secure.netroedge.com/~lm78/"
+PKG_URL="http://ftp.gwdg.de/pub/linux/misc/lm-sensors/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_PRIORITY="optional"
+PKG_SECTION="tools"
+PKG_SHORTDESC="lm_sensors: Hardware monitoring via the SMBus"
+PKG_LONGDESC="lm_sensors is a package to get data from the SMB (System Management Bus - an i2c bus) on modern mainboards. It consists of kernel modules and users space tools to get stuff like cpu / mb temperature, voltages, fan speed..."
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.python.script"
+PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
+
+PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
+
+# TODO: PKG_MAKE_OPTS_TARGET + ETCDIR=/storage/.xbmc/addons/tools.lm_sensors/data if one wants sensor3.conf..
+PKG_MAKE_OPTS_TARGET="PREFIX=/usr CC=$TARGET_CC AR=$TARGET_AR"
+PKG_MAKEINSTALL_OPTS_TARGET="PREFIX=/usr"
+
+pre_make_target() {
+  export CFLAGS="$TARGET_CFLAGS"
+  export CPPFLAGS="$TARGET_CPPFLAGS"
+}
+
+makeinstall_target() {
+  : # meh
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -PR $PKG_BUILD/prog/sensors/sensors $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
