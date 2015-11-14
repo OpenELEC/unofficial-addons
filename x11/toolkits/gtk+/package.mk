260835c8ab67d0d07f5af43ab548629de9456a7a
Stephan Raue <stephan@openelec.tv>
2015-11-15 00:09:59 +0100

    gtk+: update to gtk+-2.24.28
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- x11/toolkits/gtk+/package.mk
+++ x11/toolkits/gtk+/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="gtk+"
-PKG_VERSION="2.24.23"
+PKG_VERSION="2.24.28"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
