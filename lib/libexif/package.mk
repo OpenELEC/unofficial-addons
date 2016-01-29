ae3acb08590b4e044d26efac1b0205b2888c3b73
Lukas Rusak <lorusak@gmail.com>
2016-01-29 15:47:41 -0800

    libexif: build shared because chromium has hard paths for libexif.so

--- lib/libexif/package.mk
+++ lib/libexif/package.mk
@@ -31,5 +31,3 @@ PKG_LONGDESC="A library to parse an EXIF file and read the data from those tags"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared"
