c41d393e8d7ca8a3d7692a85bdd5eefa303a31de
Stephan Raue <stephan@openelec.tv>
2016-03-25 20:18:36 +0100

    pcsc-lite: update to pcsc-lite-1.8.16
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- system/pcsc-lite/package.mk
+++ system/pcsc-lite/package.mk
@@ -20,18 +20,18 @@
 ################################################################################
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="1.8.11"
+PKG_VERSION="1.8.16"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://pcsclite.alioth.debian.org/pcsclite.html"
-PKG_URL="https://alioth.debian.org/frs/download.php/file/3991/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://alioth.debian.org/frs/download.php/file/4164/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libusb"
 PKG_PRIORITY="optional"
 PKG_SECTION="service/system"
 PKG_SHORTDESC="Middleware to access a smart card using SCard API (PC/SC)"
 PKG_LONGDESC="Middleware to access a smart card using SCard API (PC/SC)"
-PKG_IS_ADDON="yes"
+PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 PKG_MAINTAINER="Stefan Saraev (seo at irc.freenode.net)"
