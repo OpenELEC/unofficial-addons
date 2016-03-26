eb1acc9b4aa6467bf9b022f4f4234d92493c94ce
Stephan Raue <stephan@openelec.tv>
2016-03-26 12:53:42 +0100

    pango: update to pango-1.40.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- x11/toolkits/pango/package.mk
+++ x11/toolkits/pango/package.mk
@@ -19,12 +19,12 @@
 ################################################################################
 
 PKG_NAME="pango"
-PKG_VERSION="1.38.1"
+PKG_VERSION="1.40.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.pango.org/"
-PKG_URL="http://ftp.gnome.org/pub/gnome/sources/pango/1.38/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://ftp.gnome.org/pub/gnome/sources/pango/1.40/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain glib cairo freetype fontconfig libX11 libXft harfbuzz"
 PKG_PRIORITY="optional"
 PKG_SECTION="x11/toolkits"
