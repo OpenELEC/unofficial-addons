22a7d57f4abe4a23256bbe6df1cf49615f303faf
Stephan Raue <stephan@openelec.tv>
2016-03-26 23:36:51 +0100

    RPi.GPIO: update to RPi.GPIO-0.6.2
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/RPi.GPIO/package.mk
+++ addons/tools/RPi.GPIO/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="RPi.GPIO"
-PKG_VERSION="0.6.1"
+PKG_VERSION="0.6.2"
 PKG_REV="1"
 PKG_ARCH="arm"
 PKG_LICENSE="MIT"
