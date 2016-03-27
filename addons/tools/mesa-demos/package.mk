a8575604905265fb7dc5e5977addb2310fc768eb
Stephan Raue <stephan@openelec.tv>
2016-03-27 05:05:36 +0200

    mesa-demos: build for i386 and x86_64, cosmetics
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/mesa-demos/package.mk
+++ addons/tools/mesa-demos/package.mk
@@ -21,7 +21,7 @@
 PKG_NAME="mesa-demos"
 PKG_VERSION="8.3.0"
 PKG_REV="0"
-PKG_ARCH="x86_64"
+PKG_ARCH="i386 x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="ftp://ftp.freedesktop.org/pub/mesa/demos/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
