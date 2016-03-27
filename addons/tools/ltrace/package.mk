c3e497776134fb23831894bcadf5fd2cf4f967fb
Stephan Raue <stephan@openelec.tv>
2016-03-27 05:06:08 +0200

    ltrace: build for all archs, cosmetics
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/ltrace/package.mk
+++ addons/tools/ltrace/package.mk
@@ -21,7 +21,7 @@
 PKG_NAME="ltrace"
 PKG_VERSION="0.7.3"
 PKG_REV="0"
-PKG_ARCH="x86_64"
+PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://ltrace.org/"
 PKG_URL="http://fossies.org/linux/misc/$PKG_NAME-$PKG_VERSION.tar.gz"
