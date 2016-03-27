0b7b75b39e3af2ef51bbc84f8f8c5c06e191645e
Stephan Raue <stephan@openelec.tv>
2016-03-27 05:06:49 +0200

    lm_sensors: update to lm_sensors-3.4.0
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/lm_sensors/package.mk
+++ addons/tools/lm_sensors/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="lm_sensors"
-PKG_VERSION="3.3.4"
+PKG_VERSION="3.4.0"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
