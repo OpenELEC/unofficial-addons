ffbd3c13a51e2244b2cf3dfc7e6d63339c6a1593
Stephan Raue <stephan@openelec.tv>
2016-03-27 05:00:07 +0200

    opencaster: build for all archs, cosmetics
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/tools/opencaster/package.mk
+++ addons/tools/opencaster/package.mk
@@ -21,7 +21,7 @@
 PKG_NAME="opencaster"
 PKG_VERSION="3.2.2"
 PKG_REV="0"
-PKG_ARCH="x86_64"
+PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.avalpa.com/the-key-values/15-free-software/33-opencaster"
 PKG_URL="http://ftp.de.debian.org/debian/pool/main/o/opencaster/opencaster_${PKG_VERSION}+dfsg.orig.tar.gz"
