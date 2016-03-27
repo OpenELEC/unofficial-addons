f293b4eb0711e3e4db138b6c11532849e4d721f4
Stephan Raue <stephan@openelec.tv>
2016-03-27 12:33:11 +0200

    streamdisc: use git checkout, cosmetics
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/service/multimedia/streamdisc/package.mk
+++ addons/service/multimedia/streamdisc/package.mk
@@ -17,12 +17,13 @@
 ################################################################################
 
 PKG_NAME="streamdisc"
-PKG_VERSION="0.1"
+PKG_VERSION="14f152c"
 PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/tilwolff/streamdisc"
-PKG_URL="https://github.com/tilwolff/streamdisc/archive/v$PKG_VERSION.tar.gz"
+PKG_GIT_URL="https://github.com/tilwolff/streamdisc.git"
+PKG_GIT_BRANCH="master"
 PKG_DEPENDS_TARGET="toolchain libbluray libdvdread"
 PKG_PRIORITY="optional"
 PKG_SECTION="service/multimedia"
@@ -36,18 +37,15 @@ PKG_ADDON_REPOVERSION="7.0"
 
 PKG_MAINTAINER="tilwolff (at) yahoo (dotcom)"
 
-unpack() {
-        tar -x -z -f $SOURCES/$PKG_NAME/v$PKG_VERSION.tar.gz -C $ROOT/$BUILD
-}
-
 makeinstall_target() {
   : # nothing to do here
 }
 
 addon() {
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -PL $(get_build_dir libdvdread)/.install_pkg/usr/lib/libdvdread.so.4 $ADDON_BUILD/$PKG_ADDON_ID/lib
+    cp -PL $(get_build_dir libdvdcss)/.install_pkg/usr/lib/libdvdcss.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib
+
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/lib
-  cp -P $PKG_BUILD/.$TARGET_NAME/src/streamdisc_server $ADDON_BUILD/$PKG_ADDON_ID/bin
-  cp -PL $(get_build_dir libdvdread)/.install_pkg/usr/lib/libdvdread.so.4 $ADDON_BUILD/$PKG_ADDON_ID/lib
-  cp -PL $(get_build_dir libdvdcss)/.install_pkg/usr/lib/libdvdcss.so.2 $ADDON_BUILD/$PKG_ADDON_ID/lib  
+    cp -P $PKG_BUILD/.$TARGET_NAME/src/streamdisc_server $ADDON_BUILD/$PKG_ADDON_ID/bin
 }
