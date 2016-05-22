17a26003092f65a85c439587b084fe76b4d187a6
Alex Deryskyba <alex@codesnake.com>
2016-05-22 17:46:09 +0300

    addons/debugging/tools/evtest: Update addon repository version for OpenELEC-8.0

--- addons/debugging/tools/evtest/package.mk
+++ addons/debugging/tools/evtest/package.mk
@@ -34,7 +34,7 @@ PKG_LONGDESC="evtest is a simple tool for input event debugging."
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_PROVIDES=""
-PKG_ADDON_REPOVERSION="7.0"
+PKG_ADDON_REPOVERSION="8.0"
 
 PKG_AUTORECONF="yes"
 
