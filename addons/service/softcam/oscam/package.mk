1ee1efec7f345abb0e6ffc7d87d31db9d2f9f6ee
Alex Deryskyba <alex@codesnake.com>
2016-05-24 18:12:41 +0300

    addons/service/softcam/oscam: Update addon repository version for OpenELEC-8.0

--- addons/service/softcam/oscam/package.mk
+++ addons/service/softcam/oscam/package.mk
@@ -19,7 +19,7 @@
 
 PKG_NAME="oscam"
 PKG_VERSION="11211"
-PKG_REV="1"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
@@ -34,7 +34,7 @@ PKG_DISCLAIMER="using oscam may be illegal in your country. if in doubt, do not 
 PKG_IS_ADDON="yes"
 PKG_ADDON_NAME="OSCam"
 PKG_ADDON_TYPE="xbmc.service"
-PKG_ADDON_REPOVERSION="7.0"
+PKG_ADDON_REPOVERSION="8.0"
 
 PKG_AUTORECONF="no"
 
