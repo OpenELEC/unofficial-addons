1fdc12176e7b956cfa4f32aded5d8a2dfe999b98
Alex Deryskyba <alex@codesnake.com>
2016-05-25 23:56:36 +0300

    addons/tools/aria2: Update addon repository version for OpenELEC-8.0

--- addons/tools/aria2/package.mk
+++ addons/tools/aria2/package.mk
@@ -18,7 +18,7 @@
 
 PKG_NAME="aria2"
 PKG_VERSION="1.21.0"
-PKG_REV="1"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://aria2.github.io/"
@@ -34,7 +34,7 @@ PKG_AUTORECONF="yes"
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_PROVIDES=""
-PKG_ADDON_REPOVERSION="7.0"
+PKG_ADDON_REPOVERSION="8.0"
 
 PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in openelec forum / irc channel"
 
