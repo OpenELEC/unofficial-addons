a6f9e40a44fb8bca81d98d4481f79ee471ae85ac
Alex Deryskyba <alex@codesnake.com>
2016-05-27 10:21:12 +0300

    addons/tools/mc: Use netbsd-curses library instead of ncurses, because ncurses package was removed

--- addons/tools/mc/package.mk
+++ addons/tools/mc/package.mk
@@ -25,7 +25,7 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.midnight-commander.org"
 PKG_URL="http://ftp.midnight-commander.org/${PKG_NAME}-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain libtool:host gettext:host glib pcre ncurses"
+PKG_DEPENDS_TARGET="toolchain libtool:host gettext:host glib pcre netbsd-curses"
 PKG_PRIORITY="optional"
 PKG_SECTION="tools"
 PKG_SHORTDESC="mc: visual file manager"
@@ -57,10 +57,10 @@ PKG_CONFIGURE_OPTS_TARGET="--sysconfdir=/storage/.kodi/addons/tools.mc/etc \
             --without-libintl-prefix \
             --with-internal-edit \
             --without-diff-viewer \
-            --with-subshell"
+            --with-subshell \
+            LIBS=-lterminfo"
 
 pre_configure_target() {
-  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/ncurses"
   export LDFLAGS=`echo $LDFLAGS | sed -e "s|-Wl,--as-needed||"`
 }
 
