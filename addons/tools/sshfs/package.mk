881813a3fc46ff7e4b786c0a0f092ee06d20116c
Alex Deryskyba <alex@codesnake.com>
2016-05-28 16:31:55 +0300

    addons/tools/sshfs: Update addon repository version for OpenELEC-8.0

--- addons/tools/sshfs/package.mk
+++ addons/tools/sshfs/package.mk
@@ -20,7 +20,7 @@
 
 PKG_NAME="sshfs"
 PKG_VERSION="2.7"
-PKG_REV="1"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libfuse/sshfs"
@@ -35,7 +35,7 @@ PKG_DISCLAIMER="this is an unofficial addon. please don't ask for support in ope
 PKG_IS_ADDON="yes"
 PKG_ADDON_TYPE="xbmc.python.script"
 PKG_ADDON_PROVIDES=""
-PKG_ADDON_REPOVERSION="7.0"
+PKG_ADDON_REPOVERSION="8.0"
 
 PKG_AUTORECONF="no"
 
