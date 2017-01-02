a44354c49e4bed41a6315b92c716b8da44e82631
Stephan Raue <stephan@openelec.tv>
2017-01-02 12:15:22 +0100

    pcsc-lite: update to pcsc-lite-1.8.20
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- depends/sysutils/pcsc-lite/package.mk
+++ depends/sysutils/pcsc-lite/package.mk
@@ -20,12 +20,12 @@
 ################################################################################
 
 PKG_NAME="pcsc-lite"
-PKG_VERSION="1.8.16"
+PKG_VERSION="1.8.20"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://pcsclite.alioth.debian.org/pcsclite.html"
-PKG_URL="https://alioth.debian.org/frs/download.php/file/4164/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://alioth.debian.org/frs/download.php/file/4203/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libusb"
 PKG_PRIORITY="optional"
 PKG_SECTION="service/system"
