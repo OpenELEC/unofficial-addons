04d9d8d1b8d42da2c48b89bd0ebf4395d6370de9
Stephan Raue <stephan@openelec.tv>
2017-01-02 12:02:00 +0100

    libzen: update to libzen-0.4.34
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- depends/devel/libzen/package.mk
+++ depends/devel/libzen/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libzen"
-PKG_VERSION="0.4.33"
+PKG_VERSION="0.4.34"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
