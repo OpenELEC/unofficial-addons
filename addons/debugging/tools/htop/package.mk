27a47f60ef051e53b5b20fbfe4be598c38ff5c1a
Stephan Raue <stephan@openelec.tv>
2017-01-08 13:04:37 +0100

    htop: update to htop-2.0.2, update license headers, rebuild for netbsd-curses
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- addons/debugging/tools/htop/package.mk
+++ addons/debugging/tools/htop/package.mk
@@ -1,31 +1,29 @@
 ################################################################################
 #      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      Copyright (C) 2009-2017 Stephan Raue (stephan@openelec.tv)
 #
-#  This Program is free software; you can redistribute it and/or modify
+#  OpenELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  OpenELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.tv; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="htop"
-PKG_VERSION="2.0.1"
-PKG_REV="1"
+PKG_VERSION="2.0.2"
+PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://htop.sourceforge.net/"
 PKG_URL="http://hisham.hm/htop/releases/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain ncurses"
+PKG_DEPENDS_TARGET="toolchain netbsd-curses"
 PKG_PRIORITY="optional"
 PKG_SECTION="debug/tools"
 PKG_SHORTDESC="htop: Htop is an ncurses based interactive process viewer for Linux."
@@ -40,19 +38,20 @@ PKG_AUTORECONF="yes"
 
 PKG_MAINTAINER="unofficial.addon.pro"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_malloc_0_nonnull=yes \
-            ac_cv_func_realloc_0_nonnull=yes \
-            --enable-cgroup \
-            --disable-vserver \
-            --disable-unicode \
-            --enable-proc \
-            --disable-hwloc \
-            --with-gnu-ld"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_curses_refresh=yes \
+                           --enable-cgroup \
+                           --disable-vserver \
+                           --disable-unicode \
+                           --enable-proc \
+                           --disable-hwloc \
+                           --with-gnu-ld"
 
 pre_configure_target() {
 # htop fails to build in subdirs
   cd $ROOT/$PKG_BUILD
     rm -rf .$HOST_NAME
+
+  export LIBS="-lncurses -lterminfo"
 }
 
 makeinstall_target() {
